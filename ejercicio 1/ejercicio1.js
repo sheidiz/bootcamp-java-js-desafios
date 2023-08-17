4f629ed34dd25b84e85f5746e8a874a80a270824
Sheila Diz <dizsheila07@gmail.com>
2023-08-16 22:53:03 -0300

    M4 ejercicio 1

---
+++ ejercicio 1/ejercicio1.js
@@ -0,0 +1,99 @@
+//EJERCICIO PARTE 1
+function obtenerIngresos(){
+    let ingresos = [
+        document.getElementById('ingreso-enero').value,
+        document.getElementById('ingreso-febrero').value,
+        document.getElementById('ingreso-marzo').value,
+        document.getElementById('ingreso-abril').value,
+        document.getElementById('ingreso-mayo').value,
+        document.getElementById('ingreso-junio').value,
+        document.getElementById('ingreso-julio').value,
+        document.getElementById('ingreso-agosto').value,
+        document.getElementById('ingreso-septiembre').value,
+        document.getElementById('ingreso-octubre').value,
+        document.getElementById('ingreso-noviembre').value,
+        document.getElementById('ingreso-diciembre').value
+    ];
+    return ingresos;
+}
+function obtenerPerdidas(){
+    let egresos = [
+        document.getElementById('egreso-enero').value,
+        document.getElementById('egreso-febrero').value,
+        document.getElementById('egreso-marzo').value,
+        document.getElementById('egreso-abril').value,
+        document.getElementById('egreso-mayo').value,
+        document.getElementById('egreso-junio').value,
+        document.getElementById('egreso-julio').value,
+        document.getElementById('egreso-agosto').value,
+        document.getElementById('egreso-septiembre').value,
+        document.getElementById('egreso-octubre').value,
+        document.getElementById('egreso-noviembre').value,
+        document.getElementById('egreso-diciembre').value
+    ];
+    return egresos;
+}
+function situacion(ingresos,egresos){
+    const sumaIngresos = ingresos.reduce((prev,curr) => Number(prev) + Number(curr), 0);
+    console.log(sumaIngresos);
+    const sumaEgresos = egresos.reduce((prev,curr) => Number(prev) + Number(curr), 0);
+    console.log(sumaEgresos);
+
+    if(sumaEgresos > sumaIngresos){
+        return -1;
+    }else if(sumaIngresos > sumaEgresos){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+function generaPerdidas(){
+    const ingresos = obtenerIngresos();
+    const egresos = obtenerPerdidas();
+
+    switch(situacion(ingresos,egresos)){
+        case -1:
+            document.getElementById('respuesta').innerHTML = "Hay perdidas.";
+            break;
+        case 1:
+            document.getElementById('respuesta').innerHTML = "Hay ganancias.";
+            break;
+        case 0:
+            document.getElementById('respuesta').innerHTML = "Egresos iguales a ganancias.";
+            break;
+    }
+}
+
+
+// EJERCICIO PARTE 2
+function calcularInteres(capital,plazo,tasa){
+    return (capital * plazo * (tasa/100)) / 100;
+}
+function cargarPlan(nombre){
+    const nombrePlan = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+    const capital = document.getElementById(nombre + '-capital').innerText;
+    const plazo = document.getElementById(nombre + '-plazo').innerText.slice(0,-5);
+    const tasa = document.getElementById(nombre + '-tasa').innerText.slice(0,-1);
+    const interes = calcularInteres(capital,plazo,tasa);
+
+    const plan = {
+        nombre: nombrePlan,
+        capital,
+        tasa,
+        interes
+    }
+    return plan;
+}
+function mostrarInteres(){
+    const plan001 = cargarPlan('plan-001');
+    const plan002 = cargarPlan('plan-002');
+    const plan003 = cargarPlan('plan-003');
+    const planes = [plan001,plan002,plan003];
+
+    let resultado = "";
+    planes.forEach(plan => {
+        resultado += `<h3>${plan.nombre} -> $${plan.interes} de interés </h3> `
+    });
+    
+    document.getElementById('respuesta2').innerHTML = resultado;
+}
